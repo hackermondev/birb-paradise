7381af97b909cd58f1cab8151eee638ee92ccfd0
grav4029 <grav4029@gmail.com>
2022-02-27 14:49:46 -0800

    feat: restart everything and use sapphire

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { Client, Intents, MessageReaction, MessageEmbed } = require('discord.js')
 const Discord = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
 const fs = require('fs');
-const { prefix } = require('./config.json');
+const { prefix } = require('../config.json');
 require('dotenv').config();
 
 client.commands = new Discord.Collection();
