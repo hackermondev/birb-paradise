893d7d6c8fe6b1b88dc556ae6438a159c93d152d
grav4029 <grav4029@gmail.com>
2022-02-27 14:40:00 -0800

    feat: eval command

---
+++ commands/developer/eval.js
@@ -0,0 +1,39 @@
+const util = require('util');
+// const hastebin = require('hastebin'); // removed because hastebin not imported
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	name: 'eval',
+	aliases: ['ev','e','execute'],
+	description: 'Evaluates code',
+	permissions: 'client.application.owner',
+	
+	async execute(client, message, args) {
+		if (!client.application.owner) await client.application.fetch();
+		if ((message.author != client.application.owner) && (message.author.id != '671780654882619393')) return message.delete();
+		let code = args.join(' ');
+		let includesHide = false;
+		if (code.toLowerCase().includes('--hide')) {
+			const indexOfHide = code.indexOf('--');
+			code = code.slice(0, indexOfHide-1);
+			includesHide = true;
+		}
+		if(!code) return message.reply({embeds: [new MessageEmbed().setDescription('\`code\` is a required argument that is missing').setColor('RED')]}).then(reply => setTimeout( function() { reply.delete(); message.delete()}, 3000));
+		let evaluation = await message.reply('Evaluating...')
+		try {
+			output = await eval(code);
+			type = typeof(output);
+		}
+		catch(err) {
+			return evaluation.edit(`An error occured whilst trying to evaluate your code: ${err}`)
+		}
+		if (typeof output !== 'string') output = util.inspect(output, { depth: 0 });
+		if (output.length >= 2000) {
+			return evaluation.edit(`Content was too long to be sent: ${(await hastebin.createPaste(output)).toString()}`);
+		}
+		else if (includesHide) {
+			return evaluation.delete();
+		}
+		return evaluation.edit(`Output: \`\`\`js\n${output}\`\`\`\nType: \`${type}\``);
+	},
+}
