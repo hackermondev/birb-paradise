33726e16024a67ae474c1bd671cde470266fed81
grav4029 <grav4029@gmail.com>
2022-02-27 14:52:16 -0800

    feat: ping command

---
+++ src/commands/ping.js
@@ -0,0 +1,24 @@
+const { Command } = require('@sapphire/framework');
+class PingCommand extends Command {
+  constructor(context, options) {
+    super(context, {
+      ...options,
+      name: 'ping',
+      aliases: ['pong'],
+      description: 'Gets you the latency of the bot'
+    });
+  }
+
+  async messageRun(message) {
+	const stopwatch = new Stopwatch(0).start();
+
+	const reply = await message.reply('Pinging...');
+	if (!reply) return;
+	stopwatch.stop();
+	return reply.edit(`Pong! Websocket: \`${client.ws.ping}ms\` Bot Latency: \`${stopwatch}\``)
+  }
+}
+
+module.exports = {
+  PingCommand
+};
