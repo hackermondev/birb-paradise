75130e680b98eb267cd81a92469166f4631cde93
grav4029 <grav4029@gmail.com>
2022-02-27 18:58:26 -0800

    feat: staff precondition

---
+++ src/preconditions/Staff.js
@@ -0,0 +1,8 @@
+const { Precondition } = require('@sapphire/framework');
+const { staffRoles } = require('../../config.json');
+
+class StaffPrecondition extends Precondition {
+	run(message) {
+		return staffRoles.some(role => message.member.roles.has(role)) ? this.ok() : this.error();
+	}
+}
