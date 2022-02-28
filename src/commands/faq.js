8399302f6d9c00cd5c1a14e8628ca25da0e62339
grav4029 <grav4029@gmail.com>
2022-02-27 19:20:24 -0800

    feat: faq

---
+++ src/commands/faq.js
@@ -0,0 +1,24 @@
+const { Command } = require('@sapphire/framework');
+const { faqs } = require('../../config.json');
+
+class RuleCommand extends Command {
+  constructor(context, options) {
+    super(context, {
+      ...options,
+      name: 'faq',
+      aliases: ['showfaq'],
+      preconditions: ['Staff'],
+      description: 'Displays a faq'
+    });
+  }
+  
+  messageRun(message, args) {
+	const faqNumber = args.restResult('string');
+	if (!faqNumber.success) return message.reply('You need to enter a rule number').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
+	// TODO finish faq command
+  }
+}
+
+module.exports = {
+	RuleCommand
+}
