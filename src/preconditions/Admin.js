10c8f8766e39e4629565a8b785efae17e07c327b
grav4029 <grav4029@gmail.com>
2022-02-27 17:46:19 -0800

    feat: admin precondition

---
+++ src/preconditions/Admin.js
@@ -0,0 +1,7 @@
+const { Precondition } = require('@sapphire/framework');
+
+class AdminPrecondition extends Precondition {
+	run(message) {
+		return message.member.permissions.has('ADMINISTRATOR') ? this.ok() : this.error()
+	}
+}
