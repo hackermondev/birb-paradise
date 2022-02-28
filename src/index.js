f56e50fbc2050cab0eb0b3e3c4fa5534cdd6f528
grav4029 <grav4029@gmail.com>
2022-02-27 18:19:57 -0800

    feat: add default prefix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const { SapphireClient } = require('@sapphire/framework');
+const { prefix } = require('../config.json');
 require('dotenv').config();
 
 process.on('uncaughtException', error => { console.error(error); })
 
 const client = new SapphireClient({ intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS'] });
+client.options.defaultPrefix = prefix;
 
 client.login(process.env.DISCORD_TOKEN);
