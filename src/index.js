70a517fd5429d09c468160d39f03fca9c038190c
grav4029 <grav4029@gmail.com>
2022-02-27 17:52:56 -0800

    feat: fix intents

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 
 process.on('uncaughtException', error => { console.error(error); })
 
-const client = new SapphireClient({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
+const client = new SapphireClient({ intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS'] });
 
 client.login(process.env.DISCORD_TOKEN);
