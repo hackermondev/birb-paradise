0255c7c144e4668dd1c731f4be305e0f6bec4753
grav4029 <grav4029@gmail.com>
2022-03-03 15:29:17 -0800

    feat: add token to config temporarily for sparkedhost and update import in index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { SapphireClient } = require('@sapphire/framework');
 const { prefix } = require('../config.json');
+const { temptoken } = require('../config.json');
 require('dotenv').config();
 
 process.on('uncaughtException', error => { console.error(error); })
@@ -7,4 +8,4 @@ process.on('uncaughtException', error => { console.error(error); })
 const client = new SapphireClient({ intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS'] });
 client.options.defaultPrefix = prefix;
 
-client.login(process.env.DISCORD_TOKEN);
+client.login(temptoken);
