d11526bb01236245f105a4ba96db550df6fe464c
grav4029 <grav4029@gmail.com>
2022-03-03 19:46:51 -0800

    fix: change pickresult back to restresult because that didn't work and update not working statuses

--- src/commands/faq.js
+++ src/commands/faq.js
@@ -21,8 +21,8 @@ class FaqCommand extends Command {
    */
   messageRun(message, args) {
     // return message.reply('Command not ready');
-    let faqNumber = args.pickResult('string');
-    message.channel.send('This command isn\'t ready yet, but you can still use it');
+    let faqNumber = args.restResult('string');
+    message.channel.send('This command isn\'t ready yet, but you can still use it. However, it may not work as expected');
     if (!faqNumber.success) return message.reply('You need to enter a rule number').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
     else if (Number.isNaN(Number.parseInt(ruleNumber))) return message.reply('That\'s not a valid number').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
     else if (ruleNumber < 0 || ruleNumber >= rules.length) return message.reply('That\'s not a valid rule number').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
