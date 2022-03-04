2e7a94845157e2c6cbf1be20270c6f791f329433
grav4029 <grav4029@gmail.com>
2022-03-03 19:40:43 -0800

    feat: faq command

--- src/commands/faq.js
+++ src/commands/faq.js
@@ -2,7 +2,7 @@ const { Command, Args } = require('@sapphire/framework');
 const { Message } = require('discord.js');
 const { faqs } = require('../../config.json');
 
-class RuleCommand extends Command {
+class FaqCommand extends Command {
   constructor(context, options) {
     super(context, {
       ...options,
@@ -20,11 +20,20 @@ class RuleCommand extends Command {
    * @returns 
    */
   messageRun(message, args) {
-    return message.reply('Command not ready');
-    // const faqNumber = args.restResult('string');
-    // if (!faqNumber.success) return message.reply('You need to enter a rule number').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
-    // // TODO finish faq command
+    // return message.reply('Command not ready');
+    let faqNumber = args.restResult('string');
+    message.channel.send('This command isn\'t ready yet, but you can still use it');
+    if (!ruleNumber.success) return message.reply('You need to enter a rule number').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
+    else if (Number.isNaN(Number.parseInt(ruleNumber))) return message.reply('That\'s not a valid number').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
+    else if (ruleNumber < 0 || ruleNumber >= rules.length) return message.reply('That\'s not a valid rule number').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
+    faqNumber = Number.parseInt(ruleNumber);
+    const faq = faqs[faqNumber];
+    const faqEmbed = new MessageEmbed()
+      .setTitle(`Rule ${rules.indexOf(faq)}`)
+      .setDescription(faq)
+      .setFooter({text: `${message.guild.name}`})
+    return message.reply({embeds: [faqEmbed]});
   }
 }
 
-module.exports = { RuleCommand }
+module.exports = { FaqCommand }
--- src/commands/rule.js
+++ src/commands/rule.js
@@ -20,19 +20,19 @@ class RuleCommand extends Command {
    * @returns 
    */
   messageRun(message, args) {
-  // return message.reply('Command not ready');
-	let ruleNumber = args.restResult('string');
-  message.channel.send('This command isn\'t ready yet, but you can still use it');
-	if (!ruleNumber.success) return message.reply('You need to enter a rule number').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
-	else if (Number.isNaN(Number.parseInt(ruleNumber))) return message.reply('That\'s not a valid number').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
-  else if (ruleNumber < 0 || ruleNumber >= rules.length) return message.reply('That\'s not a valid rule number').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
-  ruleNumber = Number.parseInt(ruleNumber);
-  const rule = rules[ruleNumber];
-  const ruleEmbed = new MessageEmbed()
-    .setTitle(`Rule ${rules.indexOf(rule)}`)
-    .setDescription(rule)
-    .setFooter({text: `${message.guild.name}`})
-  return message.reply({embeds: [ruleEmbed]});
+    // return message.reply('Command not ready');
+    let ruleNumber = args.restResult('string');
+    message.channel.send('This command isn\'t ready yet, but you can still use it');
+    if (!ruleNumber.success) return message.reply('You need to enter a rule number').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
+    else if (Number.isNaN(Number.parseInt(ruleNumber))) return message.reply('That\'s not a valid number').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
+    else if (ruleNumber < 0 || ruleNumber >= rules.length) return message.reply('That\'s not a valid rule number').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
+    ruleNumber = Number.parseInt(ruleNumber);
+    const rule = rules[ruleNumber];
+    const ruleEmbed = new MessageEmbed()
+      .setTitle(`Rule ${rules.indexOf(rule)}`)
+      .setDescription(rule)
+      .setFooter({text: `${message.guild.name}`})
+    return message.reply({embeds: [ruleEmbed]});
   }
 }
 
