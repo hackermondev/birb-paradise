eca49290f8859992df003c58c3e0c954e433a56b
grav4029 <grav4029@gmail.com>
2022-03-03 16:56:52 -0800

    feat: help command but idk if it works

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,4 +1,4 @@
-const { Command } = require('@sapphire/framework');
+const { Command, Args } = require('@sapphire/framework');
 const { Message } = require('discord.js');
 class HelpCommand extends Command {
   constructor(context, options) {
@@ -14,10 +14,43 @@ class HelpCommand extends Command {
   /**
    * 
    * @param { Message } message 
+   * @param { Args } args
    * @returns 
    */
-  async messageRun(message) {
-	
+  async messageRun(message, args) {
+	const commandsData = [];
+	const { commands } = this.container.stores.get('commands');
+	const command = args.pickResult('string');
+		
+	if (!command.success) {
+		commands.filter((cmd) => cmd.name != 'eval').forEach(cmd => commandsData.push(`\`${cmd.name}\``));
+		const helpEmbed = new MessageEmbed()
+			.setColor('BLUE')
+			.setTitle('Commands')
+			.setDescription(`${commandsData.toString()}`)
+			.setFooter({text: `${client.commands.size - 1} total commands. Use ${prefix}help [command] to get information on a specific command`})
+		return message.reply({embeds: [helpEmbed]});
+	}
+	const name = args[0].toLowerCase();
+
+	if (!command) {
+		return message.reply(`No help found for command \`${command.name}\``);
+	}
+	else if ((command.name == eval) && (message.author != client.application.owner)) {
+		return message.reply(`This command is too cool for you to get help on`);
+	}
+
+	commandsData.push(`**Name:** ${command.name}\n`);
+
+	if (command.aliases) commandsData.push(` **Aliases:** ${command.aliases.join(', ')}\n`);
+	if (command.description) commandsData.push(` **Description:** ${command.description}\n`);
+	if (command.usage) commandsData.push(` **Usage:** ${command.usage}\n`);
+	const commandsDataString = commandsData.join(' ');
+	const commandHelpEmbed = new MessageEmbed()
+			.setColor('BLUE')
+			.setTitle(`Information for ${command.name}`)
+			.setDescription(`${commandsDataString}`)
+	return message.reply({embeds: [commandHelpEmbed]});
   }
 }
 
