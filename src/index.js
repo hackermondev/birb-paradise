aab8a33fef36d7975568359e930c204e86f22a1a
grav4029 <grav4029@gmail.com>
2022-03-03 16:41:06 -0800

    feat: update token in convig and remove dotenv import

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const { SapphireClient } = require('@sapphire/framework');
 const { prefix } = require('../config.json');
-const { temptoken } = require('../config.json');
-require('dotenv').config();
+const { DISCORD_TOKEN } = require('../config.json');
 
 process.on('uncaughtException', error => { console.error(error); })
 
 const client = new SapphireClient({ intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS'] });
 client.options.defaultPrefix = prefix;
 
-client.login(temptoken);
+client.login(DISCORD_TOKEN);
