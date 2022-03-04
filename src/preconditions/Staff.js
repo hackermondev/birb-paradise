0c9182c580737e8e31355870254a91905628f916
grav4029 <grav4029@gmail.com>
2022-03-03 17:02:00 -0800

    fix: roles.has => roles.cache.has

--- src/preconditions/Staff.js
+++ src/preconditions/Staff.js
@@ -1,9 +1,15 @@
 const { Precondition } = require('@sapphire/framework');
+const { Message } = require('discord.js');
 const { staffRoles } = require('../../config.json');
 
 class StaffPrecondition extends Precondition {
+	/**
+	 * 
+	 * @param { Message } message 
+	 * @returns 
+	 */
 	run(message) {
-		return staffRoles.some(role => message.member.roles.has(role)) ? this.ok() : this.error();
+		return staffRoles.some(role => message.member.roles.cache.has(role)) ? this.ok() : this.error();
 	}
 }
 module.exports = { StaffPrecondition };
