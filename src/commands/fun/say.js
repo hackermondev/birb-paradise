4bdbc3707c7da4ee889af6687cc669f1069de602
grav4029 <grav4029@gmail.com>
2022-03-05 11:06:48 -0800

    fix: delete after send

--- src/commands/fun/say.js
+++ src/commands/fun/say.js
@@ -21,8 +21,8 @@ class SayCommand extends Command {
     const whatToSay = await args.restResult('string');
     if (!whatToSay.success) return message.reply('You need to tell me what to say smh').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
     if (whatToSay.value.length > 100) return message.reply('Why is your message so long').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
-    message.delete();
-    message.channel.send(whatToSay.value);
+    await message.channel.send(whatToSay.value);
+    return message.delete();
   }
 }
 
