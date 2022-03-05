b97e7eaa747e15dc2141b51d16ac0a17a462bd06
grav4029 <grav4029@gmail.com>
2022-03-05 09:53:28 -0800

    feat: guildBan listener

---
+++ src/listeners/guildBanAddEvidence.js
@@ -0,0 +1,30 @@
+const { Listener } = require('@sapphire/framework');
+const { GuildBan, Message, MessageEmbed } = require('discord.js');
+const { evidenceChannel } = require('../../config.json');
+
+class guildBanAddEvidenceListener extends Listener {
+	constructor(context, options) {
+    super(context, {
+      ...options,
+      once: true,
+      event: 'guildBanAdd'
+    });
+  }
+
+	/**
+	 * 
+	 * @param { GuildBan } ban 
+	 */
+	run(ban) {
+    const banEmbed = new MessageEmbed()
+      .setTitle('User Banned')
+      .setColor('RED')
+      .addField('User', `<@${ban.user.id}> (${ban.user.tag})`)
+      .addField('Moderator', `still have to do this`)
+      .setFooter({text: `User ID: ${ban.user.id} - ${ban.guild.name}`});
+
+    return ban.guild.channels.cache.get(evidenceChannel).send({embeds: [banEmbed]});
+	}
+}
+
+module.exports = { guildBanAddEvidenceListener };
--- src/listeners/guildMemberAdd.js
+++ src/listeners/guildMemberAdd.js
@@ -2,6 +2,14 @@ const { Listener } = require('@sapphire/framework');
 const { GuildMember } = require('discord.js');
 
 class guildMemberAddListener extends Listener {
+  constructor(context, options) {
+      super(context, {
+        ...options,
+        once: true,
+        event: 'guildMemberAdd'
+      });
+    }
+
 	/**
 	 * 
 	 * @param { GuildMember} member 
