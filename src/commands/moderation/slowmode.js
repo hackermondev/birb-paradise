a1b1d1f91fe14c19e174cff7a0e3de05ee2983cf
grav4029 <grav4029@gmail.com>
2022-03-05 14:20:29 -0800

    feat: continue work on sm

--- src/commands/moderation/slowmode.js
+++ src/commands/moderation/slowmode.js
@@ -1,5 +1,6 @@
 const { Command, Args } = require('@sapphire/framework');
 const { Message, MessageEmbed } = require('discord.js');
+const { DurationFormatter } = require('@sapphire/time-utilities');
 
 class SlowmodeCommand extends Command {
   constructor(context, options) {
@@ -18,9 +19,11 @@ class SlowmodeCommand extends Command {
    * @returns 
    */
   async messageRun(message, args) {
-    return message.reply('Command not ready');
+    // return message.reply('Command not ready');
     const number = args.pickResult('number');
-    if (!number.success) return message.reply(`The current slowmode in this channel is ${message.channel.rateLimitPerUser} seconds`);
+    const formatter = new DurationFormatter();
+    if (!number.success) return message.reply(`The current slowmode in this channel is ${formatter.format(message.channel.rateLimitPerUser * 1000)}`);
+    // todo if you enter a number
   }
 }
 
