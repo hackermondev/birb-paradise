f019d38225aaf477dcbd10568a860e5f14e4e47e
grav4029 <grav4029@gmail.com>
2022-03-05 10:59:25 -0800

    feat: say command

---
+++ src/commands/fun/say.js
@@ -0,0 +1,28 @@
+const { Command, Args } = require('@sapphire/framework');
+const { Message } = require('discord.js');
+
+class SayCommand extends Command {
+	constructor(context, options) {
+		super(context, {
+		...options,
+		name: 'emmie',
+		aliases: ['birb'],
+		description: 'idk its the birb',
+		preconditions: ['Staff']
+	});
+	}
+
+  /**
+   * 
+   * @param { Message } message 
+   * @param { Args } args
+   */
+  messageRun(message, args) {
+    const whatToSay = await args.pickResult('string');
+    if (!whatToSay.success) return message.reply('You need to tell me what to say smh').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
+    message.delete();
+    message.channel.send(whatToSay.value);
+  }
+}
+
+module.exports = { EmmieCommand };
