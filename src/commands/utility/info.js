3b3fdcbad938de2e787e3adef84dc6ec7e9d3960
grav4029 <grav4029@gmail.com>
2022-03-05 14:27:16 -0800

    feat: add server field to info embed

--- src/commands/utility/info.js
+++ src/commands/utility/info.js
@@ -24,8 +24,9 @@ class InfoCommand extends Command {
       .setColor('RANDOM')
       .addField('Memory Usage', `\`${process.memoryUsage.rss() / 1024 / 1024} MiB\``, true)
       .addField('Users Cached', `${this.container.client.users.cache.size}`, true)
-      .addField('Uptime', `${formatter.format(this.container.client.uptime)}`)
+      .addField('Uptime', `${formatter.format(this.container.client.uptime)}`, true)
       .addField('Commands', `${this.container.stores.get('commands').size}`, true)
+      .addField('Server', `${message.guild.name}`);
     return message.reply({embeds: [info]});
   }
 }
