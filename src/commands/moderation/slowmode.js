48599bbf448ac07592c6c97051ca05d361eae11a
grav4029 <grav4029@gmail.com>
2022-03-06 12:03:22 -0800

    fix: add check for neg sm

--- src/commands/moderation/slowmode.js
+++ src/commands/moderation/slowmode.js
@@ -23,6 +23,7 @@ class SlowmodeCommand extends Command {
     const number = await args.pickResult('number');
     const formatter = new DurationFormatter();
     if (!number.success) return message.reply(`The current slowmode in this channel is ${formatter.format(message.channel.rateLimitPerUser * 1000)}`);
+    if (number.value < 0) return message.reply(`The slowmode can\'t be negative`).then(reply => setTimeout(function() { reply.delete(); message.delete() }, 3500));
     if (number.value > 21600) return message.reply(`You may not set the slowmode to more than 6h`).then(reply => setTimeout(function() { reply.delete(); message.delete() }, 3500));
     message.channel.setRateLimitPerUser(number.value);
     return message.reply(`Set the slowmode in this channel to ${formatter.format(number.value * 1000)}`);
