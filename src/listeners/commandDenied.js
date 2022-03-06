6b06189fe7ee2f9c3b1cb6089c0febac32cc51c0
grav4029 <grav4029@gmail.com>
2022-03-06 10:48:21 -0800

    feat: add logging for commanddenied

--- src/listeners/commandDenied.js
+++ src/listeners/commandDenied.js
@@ -1,8 +1,15 @@
 const { Listener } = require('@sapphire/framework');
 
 class CommandDeniedListener extends Listener {
+
+  /**
+   * 
+   * @param { Error } error 
+   * @param { String } message 
+   */
   run(error, { message }) {
     message.delete();
+    console.log(`error name logging: ${error.name}`);
   }
 }
 
