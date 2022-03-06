c0e46fae768b98c38c47b2a578c5c05b65404d41
grav4029 <grav4029@gmail.com>
2022-03-06 09:35:55 -0800

    feat: message edit logging

--- src/listeners/messageEditLogging.js
+++ src/listeners/messageEditLogging.js
@@ -1,22 +1,37 @@
 const { Listener } = require('@sapphire/framework');
-const { GuildMember } = require('discord.js');
+const { Message, MessageEmbed, WebhookClient } = require('discord.js');
+const { msgLogWebhookID, msgLogWebhookToken } = require('../../config.json');
 
 class MessageEditLogging extends Listener {
   constructor(context, options) {
       super(context, {
         ...options,
-        once: true,
-        event: 'guildMemberAdd'
+        once: false,
+        event: 'messageUpdate'
       });
     }
 
 	/**
 	 * 
-	 * @param { GuildMember } member 
+	 * @param { Message } oldMessage
+	 * @param { Message } newMessage 
 	 */
-	run(message) {
-		// TODO event handler
+	run(oldMessage, newMessage) {
+		if (oldMessage.guildId !== '891286303574994974') return;
+		if (oldMessage.channel.parentId === '891307974948184114') return;
+		if (!oldMessage.content) return; // TODO add support for image logging and other types of messages
+		const webhookClient = new WebhookClient({id: msgLogWebhookID, token: msgLogWebhookToken});
+		const msgDeleteEmbed = new MessageEmbed()
+			.setTitle(`Message edited by ${oldMessage.author.tag}`)
+			.addField('Old Message', `${oldMessage.content}`)
+			.addField('New Message', `${newMessage.content}`)
+			.addField('Channel', `<#${oldMessage.channel.id}>`, true)
+			.addField('Time', `<t:${Math.round(Date.now() / 1000)}>`, true)
+			.setColor('DARK_PURPLE')
+		webhookClient.send({
+			embeds: [msgDeleteEmbed]
+		});
 	}
 }
 
-// module.exports = { }; // TODO
+module.exports = { MessageEditLogging };
