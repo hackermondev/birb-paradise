f405847dc94845af7eb660c8892f0c3875a13fdd
grav4029 <grav4029@gmail.com>
2022-03-06 12:22:41 -0800

    feat: finish kick if under 1d event

--- src/listeners/guildMemberAddAccountAgeKick.js
+++ src/listeners/guildMemberAddAccountAgeKick.js
@@ -1,5 +1,7 @@
 const { Listener } = require('@sapphire/framework');
-const { GuildMember, MessageEmbed } = require('discord.js');
+const { GuildMember, MessageEmbed, WebhookClient } = require('discord.js');
+const { accountAgeLogWebhookID } = require('../../config.json');
+const { accountAgeLogWebhookToken } = require('../../config.json');
 
 class GuildMemberAddAccountAgeKickListener extends Listener {
   constructor(context, options) {
@@ -18,11 +20,18 @@ class GuildMemberAddAccountAgeKickListener extends Listener {
 		const accountAgeKickEmbed = new MessageEmbed()
 			.setTitle(`Your account was kicked from ${member.guild.name} for having an account age below the threshold`)
 			.setDescription(`Try joining again in a few days`)
+		const accountAgeLogEmbed = new MessageEmbed()
+			.setTitle('A user was kicked since their account was too young')
+			.addField('User', `<@${member.id}>`)
+			.addField('Time', `<t:${Date.now() / 1000}>`)
+			.addField('Account Created',`<t:${member.user.createdTimestamp}:R>`)
 		if ((Date.now() - member.user.createdAt) < 86400000) {
 			await member.send({embeds: [accountAgeKickEmbed]}).catch(() => {});
 			await member.kick("Account was less than 1d old");
-			await 
+			const webhookClient = new WebhookClient({id: accountAgeLogWebhookID, token: accountAgeLogWebhookToken});
+			return webhookClient.send({embeds: [accountAgeLogEmbed]})  
 		}
+		else return;
 	}
 }
 
