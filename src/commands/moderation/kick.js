3bfeceb21fc3b5f9624dae6207bc3957f7ae44ce
grav4029 <grav4029@gmail.com>
2022-03-06 19:04:49 -0800

    feat: work on kick cmd

--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -7,7 +7,7 @@ class KickCommand extends Command {
       ...options,
       name: 'kick',
       aliases: ['k'],
-      description: 'Kicks a member from the server',
+      description: 'Kicks a member from the server(doesn\'t currently log anything',
       preconditions: ['Staff']
     });
   }
@@ -20,7 +20,11 @@ class KickCommand extends Command {
    */
   async messageRun(message, args) {
     return message.reply('Command isn\'t ready yet');
-    const member = args.pickResult('member');
+    const rawMember = await args.pickResult('string');
+    if (!rawMember.success) return message.reply('You must provide a member to kick').then(reply => setTimeout(function() { reply.delete(); message.delete() }, 3500));
+    const member = await args.pickResult('member');
+    if (!member.success) return message.reply('Mention a valid user to kick').then(reply => setTimeout(function() { reply.delete(); message.delete() }, 3500));
+    await member.value.kick() // TODO reason and stuff
   }
 }
 
