f964d985c5448388793f0a71ef550d3afe3ceda0
grav4029 <grav4029@gmail.com>
2022-03-06 20:07:31 -0800

    chore: run prettier

--- src/commands/moderation/timeout.js
+++ src/commands/moderation/timeout.js
@@ -1,31 +1,42 @@
-const { Command, Args } = require('@sapphire/framework');
-const { Message, MessageEmbed } = require('discord.js');
-const { Duration } = require('@sapphire/time-utilities');
+const { Command, Args } = require("@sapphire/framework");
+const { Message, MessageEmbed } = require("discord.js");
+const { Duration } = require("@sapphire/time-utilities");
 
 class TimeoutCommand extends Command {
-	constructor(context, options) {
+  constructor(context, options) {
     super(context, {
       ...options,
-      name: 'timeout',
-      preconditions: ['Staff'],
-      description: 'Times out someone in the server'
+      name: "timeout",
+      preconditions: ["Staff"],
+      description: "Times out someone in the server",
     });
   }
 
   /**
-   * 
-   * @param { Message } message 
-   * @param { Args } args 
+   *
+   * @param { Message } message
+   * @param { Args } args
    */
   async messageRun(message, args) {
-    return message.reply('Command isn\'t ready yet');
-    const rawArgs = await args.restResult('string');
-    if (!rawArgs.success) return message.reply('Mention someone to timeout').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
-    const actionMember = await args.pickResult('member');
-    const rawActionTime = await args.pickResult('string');
+    return message.reply("Command isn't ready yet");
+    const rawArgs = await args.restResult("string");
+    if (!rawArgs.success)
+      return message.reply("Mention someone to timeout").then((reply) =>
+        setTimeout(function () {
+          message.delete();
+          reply.delete();
+        }, 3500)
+      );
+    const actionMember = await args.pickResult("member");
+    const rawActionTime = await args.pickResult("string");
     // actionTime.
-    if (!actionMember.success) return message.reply('Mention a valid user to timeout').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
-
+    if (!actionMember.success)
+      return message.reply("Mention a valid user to timeout").then((reply) =>
+        setTimeout(function () {
+          message.delete();
+          reply.delete();
+        }, 3500)
+      );
   }
 }
 
