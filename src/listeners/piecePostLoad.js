2ab059129e2987703a59e86dc44650b59d1faf14
grav4029 <grav4029@gmail.com>
2022-03-07 18:29:12 -0800

    feat: piecePostLoad listener

---
+++ src/listeners/piecePostLoad.js
@@ -0,0 +1,24 @@
+const { Listener, Store, Piece } = require("@sapphire/framework");
+
+class PiecePostLoadListener extends Listener {
+  constructor(context, options) {
+    super(context, {
+      ...options,
+      name: "piecePostLoad",
+      once: false,
+      event: "piecePostLoad",
+    });
+  }
+
+  /**
+   *
+   * @param { Store } store
+   * @param { Piece } piece
+   */
+  run(store, piece) {
+    // TODO event
+    this.container.logger.info(`Loaded command ${piece.name}`);
+  }
+}
+
+module.exports = { CommandFinishListener };
