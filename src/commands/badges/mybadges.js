9f8fc7c490b4774724f2bc81e4098c7300a13fd5
grav4029 <grav4029@gmail.com>
2022-03-12 11:54:19 -0800

    chore: prettier run after changes

--- src/commands/badges/mybadges.js
+++ src/commands/badges/mybadges.js
@@ -1,8 +1,8 @@
-const { Command } = require('@sapphire/framework');
-const { Message } = require('discord.js');
+const { Command } = require("@sapphire/framework");
+const { Message } = require("discord.js");
 
 class MyBadgesCommand extends Command {
-	constructor(context, options) {
+  constructor(context, options) {
     super(context, {
       ...options,
       name: "mybadges",
@@ -13,19 +13,28 @@ class MyBadgesCommand extends Command {
   }
 
   /**
-   * 
-   * @param { Message } message 
+   *
+   * @param { Message } message
    */
   messageRun(message) {
     let badges = message.author.flags.toArray().toString();
-		badges = badges.replace('DISCORD_EMPLOYEE', 'Discord Staff');
-		badges = badges.replace('HOUSE_BRILLIANCE', 'Hypesquad Brilliance');
-		badges = badges.replace('HOUSE_BALANCE', 'Hypesquad Balance');
-		badges = badges.replace('EARLY_VERIFIED_BOT_DEVELOPER', 'Early Verified Bot Developer');
-		badges = badges.replace('DISCORD_CERTIFIED_MODERATOR', 'Discord Certified Moderator');
-		const badgesArray = badges.split(',');
-    if (!badgesArray.length) return message.reply('You have no badges');
-		else return message.reply(`These are the badges you have: ${badgesArray.toString()}`);
+    badges = badges.replace("DISCORD_EMPLOYEE", "Discord Staff");
+    badges = badges.replace("HOUSE_BRILLIANCE", "Hypesquad Brilliance");
+    badges = badges.replace("HOUSE_BALANCE", "Hypesquad Balance");
+    badges = badges.replace(
+      "EARLY_VERIFIED_BOT_DEVELOPER",
+      "Early Verified Bot Developer"
+    );
+    badges = badges.replace(
+      "DISCORD_CERTIFIED_MODERATOR",
+      "Discord Certified Moderator"
+    );
+    const badgesArray = badges.split(",");
+    if (!badgesArray.length) return message.reply("You have no badges");
+    else
+      return message.reply(
+        `These are the badges you have: ${badgesArray.toString()}`
+      );
   }
 }
 
--- src/commands/badges/whohasbadge.js
+++ src/commands/badges/whohasbadge.js
@@ -1,9 +1,9 @@
-const { Command, Args } = require('@sapphire/framework');
-const { Message } = require('discord.js');
-const req = require('petitio');
+const { Command, Args } = require("@sapphire/framework");
+const { Message } = require("discord.js");
+const req = require("petitio");
 
 class WhoHasBadgeCommand extends Command {
-	constructor(context, options) {
+  constructor(context, options) {
     super(context, {
       ...options,
       name: "whohasbadge",
@@ -14,26 +14,78 @@ class WhoHasBadgeCommand extends Command {
   }
 
   /**
-   * 
-   * @param { Message } message 
+   *
+   * @param { Message } message
    * @param { Args } args
    */
   async messageRun(message, args) {
-    const rawBadge = await args.restResult('string');
-    if (!rawBadge.success) return message.reply('You must provide a badge to search for').then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
-    const badgesStrings = ['discord staff','hypesquad brilliance','hypesquad balance','hypesquad bravery','early verified bot developer','discord certified moderator','hypesquad events','early supporter','partner'];
-		const badgesLiteralStrings = ['DISCORD_EMPLOYEE','HOUSE_BRILLIANCE','HOUSE_BALANCE','HOUSE_BRAVERY','EARLY_VERIFIED_BOT_DEVELOPER','DISCORD_CERTIFIED_MODERATOR','HYPESQUAD_EVENTS','EARLY_SUPPORTER','PARTNERED_SERVER_OWNER'];
-    if (!badgesStrings.includes(rawBadge.value)) return message.reply(`That badge doesn\'t exist. The badges you can search for are: ${badgesStrings.toString()}`).then(reply => setTimeout(function() { message.delete(); reply.delete();}, 3500));
+    const rawBadge = await args.restResult("string");
+    if (!rawBadge.success)
+      return message
+        .reply("You must provide a badge to search for")
+        .then((reply) =>
+          setTimeout(function () {
+            message.delete();
+            reply.delete();
+          }, 3500)
+        );
+    const badgesStrings = [
+      "discord staff",
+      "hypesquad brilliance",
+      "hypesquad balance",
+      "hypesquad bravery",
+      "early verified bot developer",
+      "discord certified moderator",
+      "hypesquad events",
+      "early supporter",
+      "partner",
+    ];
+    const badgesLiteralStrings = [
+      "DISCORD_EMPLOYEE",
+      "HOUSE_BRILLIANCE",
+      "HOUSE_BALANCE",
+      "HOUSE_BRAVERY",
+      "EARLY_VERIFIED_BOT_DEVELOPER",
+      "DISCORD_CERTIFIED_MODERATOR",
+      "HYPESQUAD_EVENTS",
+      "EARLY_SUPPORTER",
+      "PARTNERED_SERVER_OWNER",
+    ];
+    if (!badgesStrings.includes(rawBadge.value))
+      return message
+        .reply(
+          `That badge doesn\'t exist. The badges you can search for are: ${badgesStrings.toString()}`
+        )
+        .then((reply) =>
+          setTimeout(function () {
+            message.delete();
+            reply.delete();
+          }, 3500)
+        );
     let index = badgesStrings.indexOf(rawBadge.value);
-    let r = await message.reply(`Fetching members and checking for the badge ${badgesLiteralStrings[index]}...`);
-    let guilds = [...client.guilds.cache.values()]
-		for (let i = 0; i < guilds.length; i++) {
-			const guild = guilds[i];
-			await guild.members.fetch();
-		}
+    let r = await message.reply(
+      `Fetching members and checking for the badge ${badgesLiteralStrings[index]}...`
+    );
+    let guilds = [...client.guilds.cache.values()];
+    for (let i = 0; i < guilds.length; i++) {
+      const guild = guilds[i];
+      await guild.members.fetch();
+    }
     let membersWithBadge = [];
-    membersWithBadge = message.guild.members.cache.filter((member) => ((member.user.flags) && (member.user.flags.toArray().toString().includes(badgesLiteralStrings[index])))).map(`${member.user.tag} (<@${member.id}>)`);
-    if (membersWithBadge.length == 0) return r.edit(`I couldn\'t find anyone who has the ${badgesLiteralStrings[index]} badge`);
+    membersWithBadge = message.guild.members.cache
+      .filter(
+        (member) =>
+          member.user.flags &&
+          member.user.flags
+            .toArray()
+            .toString()
+            .includes(badgesLiteralStrings[index])
+      )
+      .map(`${member.user.tag} (<@${member.id}>)`);
+    if (membersWithBadge.length == 0)
+      return r.edit(
+        `I couldn\'t find anyone who has the ${badgesLiteralStrings[index]} badge`
+      );
     if (membersWithBadge.toString().length > 4000) {
       const res = await req("https://hst.sh/documents", "POST")
         .body(output)
@@ -48,9 +100,12 @@ class WhoHasBadgeCommand extends Command {
           res.json().key
         }`
       );
-		}
-    else {
-      return r.edit(`The following members have the ${badgesLiteralStrings[index]} badge: ${membersWithBadge.toString()}`);
+    } else {
+      return r.edit(
+        `The following members have the ${
+          badgesLiteralStrings[index]
+        } badge: ${membersWithBadge.toString()}`
+      );
     }
   }
 }
