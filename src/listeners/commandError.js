d8d8085938bbd42b5aae285d251be3453f7b1b0d
grav4029 <grav4029@gmail.com>
2022-03-13 12:39:18 -0700

    fix: attempt a change but no success

---
+++ src/listeners/commandError.js
@@ -0,0 +1,22 @@
+const { Listener, CommandErrorPayload } = require("@sapphire/framework");
+
+class ErrorListener extends Listener {
+  constructor(context, options) {
+    super(context, {
+      ...options,
+      name: "error",
+      once: false,
+      event: "CommandError",
+    });
+  }
+  /**
+   *
+   * @param { Error } error
+   * @param { CommandErrorPayload } payload
+   */
+  async run(error, payload) {
+   this.container.logger.debug(`payload message: ${payload.message}, error message: ${error.message}`);
+  }
+}
+
+module.exports = { ErrorListener };
