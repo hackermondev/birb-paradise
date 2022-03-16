16b8a42711622b3ca44aeb99acf2205fea3b5240
grav4029 <grav4029@gmail.com>
2022-03-15 19:30:01 -0700

    feat: add subcommands to prefix and update help

--- src/commands/utility/prefix.js
+++ src/commands/utility/prefix.js
@@ -1,14 +1,16 @@
-const { Command } = require("@sapphire/framework");
+const { Command, Args } = require("@sapphire/framework");
+const { SubCommandPluginCommand } = require("@sapphire/plugin-subcommands");
 const { Message, MessageEmbed } = require("discord.js");
 const { prefix } = require("../../../config.json");
 
-class PrefixCommand extends Command {
+class PrefixCommand extends SubCommandPluginCommand {
   constructor(context, options) {
     super(context, {
       ...options,
       name: "prefix",
       aliases: ["currentprefix", "commandprefix"],
       description: "Gets you the prefix of the bot",
+      subCommands: ["set", "get"],
       preconditions: ["Staff"],
     });
   }
@@ -17,7 +19,7 @@ class PrefixCommand extends Command {
    *
    * @param { Message } message
    */
-  messageRun(message) {
+  async get(message) {
     return message.reply({
       embeds: [
         new MessageEmbed()
@@ -25,6 +27,28 @@ class PrefixCommand extends Command {
           .setColor("DARK_AQUA"),
       ],
     });
+  }
+
+  /**
+   *
+   * @param { Message } message
+   * @param { Args } args
+   * @returns
+   */
+  async set(message, args) {
+    const newPrefix = await args.restResult("string");
+    if (!newPrefix.success)
+      return message
+        .reply("You must provide a new prefix to set")
+        .then((reply) =>
+          setTimeout(function () {
+            message.delete();
+            reply.delete();
+          }, 3500)
+        );
+    require("../../../config.json").prefix = newPrefix;
+    this.container.client.options.defaultPrefix = newPrefix;
+    return message.reply(`Prefix successfully set to ${newPrefix}`);
   }
 }
 
