affc76d5500e70be9180f1b1c46509fa7387bcaf
grav4029 <grav4029@gmail.com>
2022-03-17 15:06:03 -0700

    feat: start on donate stuff

---
+++ src/commands/donations/donate.js
@@ -0,0 +1,22 @@
+const { Command } = require('@sapphire/framework');
+
+class DonateCommand extends Command {
+	constructor(context, options) {
+    super(context, {
+      ...options,
+      name: "donate",
+      preconditions: ["Admin"],
+      description: "command to donate",
+      enabled: false
+    });
+  }
+
+  /**
+   *
+   * @param { Message } message
+   * @param { Args } args
+   */
+  async messageRun(message, args) {
+    return message.reply('Command not ready');
+  }
+}
