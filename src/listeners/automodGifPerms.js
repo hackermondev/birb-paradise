5ee18179580d17dcc9eb8a933904263aed9b00dd
grav4029 <grav4029@gmail.com>
2022-03-17 17:57:16 -0700

    feat: work on gif perms

--- src/listeners/automodGifPerms.js
+++ src/listeners/automodGifPerms.js
@@ -1,1 +1,23 @@
-const { Listener } = require('@sapphire/framework');
+const { Listener, Events } = require('@sapphire/framework');
+const { Message } = require('discord.js');
+const { staffRoles } = require('../../config.json');
+
+class AutomodGifPermsListener extends Listener {
+	constructor(context, options) {
+        super(context, {
+            ...options,
+            name: 'automodGifPerms',
+            once: false,
+            event: Events.MessageCreate,
+        });
+    }
+
+    /**
+     * 
+     * @param { Message } message 
+     */
+    async run(message) {
+        if (staffRoles.some(role => message.member.roles.cache.has(role))) return;
+        
+    }
+}
