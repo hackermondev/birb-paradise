f8a89cedb42c111fed6e27941077b825d1b8fc1c
grav4029 <grav4029@gmail.com>
2022-03-18 21:43:38 -0700

    chore: run prettier

--- src/commands/fun/food.js
+++ src/commands/fun/food.js
@@ -2,21 +2,23 @@ const { Command } = require('@sapphire/framework');
 const { Message } = require('discord.js');
 
 class FoodCommand extends Command {
-	constructor(context, options) {
-		super(context, {
-		    ...options,
-		    name: 'food',
-		    preconditions: ['Staff'],
-		    description: 'food!',
-		});
-	}
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            name: 'food',
+            preconditions: ['Staff'],
+            description: 'food!',
+        });
+    }
 
     /**
-     * 
-     * @param { Message } message 
+     *
+     * @param { Message } message
      */
     async messageRun(message) {
-        return message.reply('https://tenor.com/view/cute-bird-parrot-perroquet-oiseau-gif-13534334');
+        return message.reply(
+            'https://tenor.com/view/cute-bird-parrot-perroquet-oiseau-gif-13534334'
+        );
     }
 }
 
