793c486c243079622529e91af79512e1e53216b6
grav4029 <grav4029@gmail.com>
2022-03-18 19:55:41 -0700

    feat: food cmd

---
+++ src/commands/fun/food.js
@@ -0,0 +1,21 @@
+const { Command } = require('@sapphire/framework');
+const { Message } = require('discord.js');
+
+class FoodCommand extends Command {
+	constructor(context, options) {
+		super(context, {
+		    ...options,
+		    name: 'food',
+		    preconditions: ['Staff'],
+		    description: 'food!',
+		});
+	}
+
+    /**
+     * 
+     * @param { Message } message 
+     */
+    async messageRun(message) {
+        return message.reply('https://tenor.com/view/cute-bird-parrot-perroquet-oiseau-gif-13534334');
+    }
+}
