ab458dd303e1231079009189b203cd9c36f49c7f
grav4029 <grav4029@gmail.com>
2022-03-21 16:32:49 -0700

    feat: start on lockdown

---
+++ src/commands/utility/lockdown.js
@@ -0,0 +1,49 @@
+const { Command } = require('@sapphire/framework');
+const { Message, MessageEmbed } = require('discord.js');
+const {
+    lockdownChannels,
+    bpGuildID,
+    mainChannel,
+} = require('../../../config.json');
+
+class LockdownCommand extends Command {
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            name: 'lockdown',
+            aliases: ['lockall'],
+            description: 'Locks down Birb Paradise',
+            preconditions: ['Staff'],
+            enabled: false,
+        });
+    }
+
+    /**
+     *
+     * @param { Message } message
+     */
+    async messageRun(message) {
+        if (message.guildId !== bpGuildID)
+            return message.reply('This server is not configured');
+        message.channel.send('Starting Lockdown...');
+        for (var i = 0; i < lockdownChannels.length; ++i) {
+            const ch = message.guild.channels.cache.get(lockdownChannels[i]);
+            await ch.permissionOverwrites.edit(message.guild.roles.everyone, {
+                SEND_MESSAGES: false,
+            });
+            await ch.send(
+                `This channel is locked. see <#${mainChannel}> for more information.`
+            );
+        }
+        // TODO handle ratelimits
+        const serverLockEmbed = new MessageEmbed()
+            .setTitle('Server Lockdown')
+            .setFooter({ text: `${message.guild.name}` });
+        message.guild.channels.cache
+            .get(mainChannel)
+            .send({ embeds: [serverLockEmbed] });
+        message.channel.send('The server has been locked down.');
+    }
+}
+
+module.exports = { LockdownCommand };
