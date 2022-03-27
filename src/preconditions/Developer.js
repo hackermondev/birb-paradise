632f53516a177ae857437559a82cae9f986c0996
grav4029 <grav4029@gmail.com>
2022-03-27 16:35:53 -0700

    fix: dev precondition v2

--- src/preconditions/Developer.js
+++ src/preconditions/Developer.js
@@ -10,7 +10,7 @@ class DeveloperPrecondition extends Precondition {
     async messageRun(message) {
         if (!this.container.client.application.owner)
             await this.container.client.application.fetch();
-        return message.author.id === this.container.client.application.owner.ownerId
+        return message.author.id === this.container.client.application.owner.id
             ? this.ok()
             : this.error('User is not a developer');
     }
