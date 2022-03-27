12593006b3a7bab16eb05d54fc57a50584aefef6
grav4029 <grav4029@gmail.com>
2022-03-27 16:34:27 -0700

    fix: dev precondition

--- src/preconditions/Developer.js
+++ src/preconditions/Developer.js
@@ -1,10 +1,16 @@
 const { Precondition } = require('@sapphire/framework');
+const { Message } = require('discord.js');
 
 class DeveloperPrecondition extends Precondition {
+    /**
+     * 
+     * @param { Message } message 
+     * @returns 
+     */
     async messageRun(message) {
         if (!this.container.client.application.owner)
             await this.container.client.application.fetch();
-        return message.author === this.container.client.application.owner
+        return message.author.id === this.container.client.application.owner.ownerId
             ? this.ok()
             : this.error('User is not a developer');
     }
