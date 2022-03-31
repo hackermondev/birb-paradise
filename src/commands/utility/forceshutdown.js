76b48a7f35bd7817e2675ab317cfced3c2924218
grav4029 <grav4029@gmail.com>
2022-03-30 17:48:22 -0700

    feat: forceshutdown cmd

---
+++ src/commands/utility/forceshutdown.js
@@ -0,0 +1,27 @@
+const { Command } = require('@sapphire/framework');
+const { Message } = require('discord.js');
+
+class ForceShutdownCommand extends Command {
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            name: 'forceshutdown',
+            preconditions: ['Admin'],
+            description: 'Forcefully shuts down Birb Helper(it won\'t restart)',
+        });
+    }
+
+    /**
+     *
+     * @param { Message } message
+     * @returns
+     */
+    async messageRun(message) {
+        message.channel.send('Shutting down...(You will be able to see that the bot is shutdown as it will go offline)');
+	this.container.logger.warn(`Force shutdown signal sent by ${message.member.user.tag}`);
+	this.container.client.destroy();
+	return this.container.logger.info('Client destroyed.');
+    }
+}
+
+module.exports = { ForceShutdownCommand };
