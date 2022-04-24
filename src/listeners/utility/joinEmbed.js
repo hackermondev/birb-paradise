f999de415005f52e4b15db278065922e303f8f56
graviscool <88552913+graviscool@users.noreply.github.com>
2022-04-24 16:05:59 -0700

    feat: send embed on join (#28)

---
+++ src/listeners/utility/joinEmbed.js
@@ -0,0 +1,37 @@
+const { Listener, Events } = require('@sapphire/framework');
+const { GuildMember, MessageEmbed, Message } = require('discord.js');
+const { memberJoinChannel } = require('../../../config.json');
+
+class GuildMemberRemoveMsg extends Listener {
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            event: Events.GuildMemberAdd,
+        });
+    }
+
+    /**
+     *
+     * @param { GuildMember } member
+     */
+    async run(member) {
+        if (!this.container.utility.isBp(member.guild)) return;
+        const joinEmbed = new MessageEmbed()
+            .setTitle('Welcome')
+            .setDescription(`Welcome to the server, ${member}!`)
+            .setColor('RANDOM')
+            .setThumbnail(
+                'https://media.discordapp.net/attachments/891286303574994977/904481443890200616/Untitled1205_20211031142614.png'
+            )
+            .setFooter({
+                iconURL: member.user.avatarURL(),
+                text: `${member.user.tag} joined`,
+            })
+            .setTimestamp();
+        return member.guild.channels.cache
+            .get(memberJoinChannel)
+            .send({ embeds: joinEmbed });
+    }
+}
+
+module.exports = { GuildMemberRemoveMsg };
